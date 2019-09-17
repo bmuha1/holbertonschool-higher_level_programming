0f658d9e2001d62918077d18086db4754fe732cb
Brent Muha <800@holbertonschool.com>
2019-09-17 23:27:50 +0000

    Add reverse linked list function

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -20,6 +20,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int get_value_at(listint_t *h, int index);
+listint_t *reverse_list(listint_t **head);
 
 #endif /* LISTS_H */
