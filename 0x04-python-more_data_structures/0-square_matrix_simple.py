b5c5abe930bc47b3bbf7d6036b2c099bd4642a3a
Brent Muha <800@holbertonschool.com>
2019-09-19 15:39:27 +0000

    Make square matrix simple one line

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squares = [[matrix[i][j]**2 for j in range(len(matrix[0]))]
-               for i in range(len(matrix))]
-    return squares
+    return [[matrix[i][j]**2 for j in range(len(matrix[0]))]
+            for i in range(len(matrix))]
