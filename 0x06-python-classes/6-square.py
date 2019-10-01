aaea4a409302226f60216ff22486c181ed6d51dc
Brent Muha <800@holbertonschool.com>
2019-10-01 22:03:48 +0000

    Add 6 square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Define a square with position coordinates."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size."""
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position."""
+        if type(value) != tuple or type(value[0]) != int or \
+           type(value[1]) != int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size**2
+
+    def my_print(self):
+        """Print the square."""
+        if self.__size == 0:
+            print()
+        else:
+            print('\n' * self.__position[1], end='')
+            for i in range(self.__size):
+                print(' ' * self.__position[0], end='')
+                print('#' * self.__size)
