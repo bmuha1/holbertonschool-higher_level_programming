f73e57e137d0069ba31ca3e58a4cd9cc4635ebf9
Brent Muha <800@holbertonschool.com>
2019-10-01 18:04:10 +0000

    Add documentation

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 class Square:
+    """Represents a square"""
+
     def __init__(self, size=0):
+        """Initializes the data"""
         self.size = size
 
     @property
     def size(self):
+        """Get the size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size"""
         if type(value) != int:
             raise TypeError('size must be an integer')
         if value < 0:
@@ -16,4 +21,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return the area of the square"""
         return self.__size**2
