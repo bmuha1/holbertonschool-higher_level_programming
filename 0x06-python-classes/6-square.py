6accb5e741ff75ebe6a6d96aab3cc33fb2aa35b0
Brent Muha <800@holbertonschool.com>
2019-10-02 00:41:13 +0000

    Ensure tuple has only two elements

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,8 +32,8 @@ class Square:
     @position.setter
     def position(self, value):
         """Set the position."""
-        if type(value) != tuple or type(value[0]) != int or \
-           type(value[1]) != int or value[0] < 0 or value[1] < 0:
+        if type(value) != tuple or len(value) != 2 or type(value[0]) != int\
+           or type(value[1]) != int or value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
