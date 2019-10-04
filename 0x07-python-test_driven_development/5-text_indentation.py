87bc615390ce4c4db1e8b00b766ca6ae207c9bcd
Brent Muha <800@holbertonschool.com>
2019-10-04 17:46:43 +0000

    Add text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is the "text indentation" module.
+
+The text indentation module supplies one function, text_indentation().
+"""
+
+
+def text_indentation(text):
+    """
+    Print a text with 2 new lines after these characters: . ? :
+    """
+
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    newline = True
+    for c in text:
+        if not (newline and c == ' '):
+            print(c, end='')
+            newline = False
+            if c in '.?:':
+                print('\n')
+                newline = True
