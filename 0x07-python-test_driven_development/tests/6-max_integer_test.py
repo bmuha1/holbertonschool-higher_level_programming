9378a890ab03efd3f230f643acda2d7082d57e40
Brent Muha <800@holbertonschool.com>
2019-10-05 01:13:49 +0000

    Add extra valid integer tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,6 +13,8 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([-2, -6, -8, -4]), -2)
         self.assertEqual(max_integer([10, -20, 30, -40]), 30)
+        self.assertEqual(max_integer([50]), 50)
+        self.assertEqual(max_integer([0, -5, -10]), 0)
 
     def test_type(self):
         """Test other compatible data types."""
