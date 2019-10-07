27ccc3a40c27852ed24156fe8522fdd5db1a7ba7
Brent Muha <800@holbertonschool.com>
2019-10-07 19:30:47 +0000

    Change initialization

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -3,8 +3,8 @@ class Rectangle:
     """Represents a rectangle."""
     def __init__(self, width=0, height=0):
         """Initialize the data."""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def __del__(self):
         """Delete a rectangle."""
