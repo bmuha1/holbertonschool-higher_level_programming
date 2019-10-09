768e6e0e05cf200585d00e3d1c44955a0bdd30bf
Brent Muha <800@holbertonschool.com>
2019-10-09 11:08:44 +0000

    Add rectangle 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Represents a BaseGeometry."""
+    pass
+
+    def area(self):
+        """Raise an exception."""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validate value."""
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Represents a Rectangle."""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __repr__(self):
+        """Print the width and height of the rectangle."""
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
