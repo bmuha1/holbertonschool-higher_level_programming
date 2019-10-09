7a357b1343ef4779c58eade046470372b44f640b
Brent Muha <800@holbertonschool.com>
2019-10-09 21:50:42 +0000

    Add class to json

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -7,5 +7,5 @@ The class to json module defines one function, class_to_json().
 
 
 def class_to_json(obj):
-    """Return the dictionary description for JSON serialization of an object."""
+    """Return the dict description for JSON serialization of an object."""
     return obj.__dict__
