2a3a1a0cba2ab3bcf13611b3753656eb26bcdfaf
Brent Muha <800@holbertonschool.com>
2019-10-09 11:03:00 +0000

    Add rectangle 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Represents a BaseGeometry."""
+    pass
+
+    def area(self):
+        """Raise an exception."""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validate value."""
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Represents a Rectangle."""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
