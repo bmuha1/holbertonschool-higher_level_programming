76695e2e37f58390598b7bc5d1e58af052220de2
Brent Muha <800@holbertonschool.com>
2019-10-09 18:35:45 +0000

    Add read lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is the "read lines" module.
+
+The read lines module defines one function, read_lines().
+"""
+
+def read_lines(filename="", nb_lines=0):
+    """Read n lines of a text file and print them."""
+    with open(filename, 'r') as f:
+        if nb_lines <= 0:
+            print(f.read(), end='')
+            return
+        count = 0
+        for line in f:
+            if count < nb_lines:
+                print(line, end='')
+            count += 1
