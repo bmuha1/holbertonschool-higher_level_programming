40bddc8d34a0e15e37a7a6e0a2ae8d9ad9304633
Brent Muha <800@holbertonschool.com>
2019-10-09 19:38:36 +0000

    Add to json string

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the "to json string" module.
+
+The to json string module defines one function, to_json_string().
+"""
+
+def to_json_string(my_obj):
+    """Return the JSON representation of an object."""
+    import json
+    return json.dumps(my_obj)
