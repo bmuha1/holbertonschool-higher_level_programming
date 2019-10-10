d91a4ac4b5d84469caab67926ecb158c35a518a8
Brent Muha <800@holbertonschool.com>
2019-10-10 00:52:55 +0000

    Check none instead of not attrs

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -15,10 +15,10 @@ class Student:
 
     def to_json(self, attrs=None):
         """Retrieve specified attributes of a Student."""
-        if not attrs or type(attrs) != list:
+        if attrs is None:
             return self.__dict__
         my_attrs = {}
-        for key in attrs:
-            if key in self.__dict__.keys():
-                my_attrs[key] = self.__dict__[key]
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_attrs[key] = value
         return my_attrs
