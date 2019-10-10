1cdae159b7636e466db6680fb902b1dd5f4a7217
Brent Muha <800@holbertonschool.com>
2019-10-10 02:48:43 +0000

    Import rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,44 +4,8 @@ This is the BaseGeometry module.
 
 The BaseGeometry module contains two functions, area() and integer_validator().
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
-
-class BaseGeometry:
-    """Represents a BaseGeometry."""
-
-    def area(self):
-        """Raise an exception."""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """Validate value."""
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
-"""
-This is the Rectangle module.
-
-The Rectangle module defines the Rectangle class.
-"""
-
-
-class Rectangle(BaseGeometry):
-    """Represents a Rectangle."""
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __repr__(self):
-        """Print the width and height of the rectangle."""
-        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
-
-    def area(self):
-        """Return the area of the rectangle."""
-        return self.__width * self.__height
 
 """
 This is the Square module.
