3021eb58828593f17ac9150f12e7cba0af91b87c
Brent Muha <800@holbertonschool.com>
2019-10-10 00:31:18 +0000

    Use floor division instead of casting to int

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -8,14 +8,14 @@ The pascal triangle module defines one function, pascal_triangle().
 
 def pascal_triangle(n):
     """Return a list of integers representing Pascal's triangle of n."""
-    list = []
+    my_list = []
     if n <= 0:
-        return list
+        return my_list
     for i in range(1, n + 1):
         value = 1
         temp_list = []
         for j in range(1, i + 1):
             temp_list.append(str(value))
-            value = int(value * (i - j) / j)
-        list.append(temp_list)
-    return list
+            value = value * (i - j) // j
+        my_list.append(temp_list)
+    return my_list
