e1118e38c695b2311ad203f6ccc87f6c90f4c465
Brent Muha <800@holbertonschool.com>
2020-01-06 20:41:46 +0000

    Add post parameters

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Write a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be he\
+re for PLD"
