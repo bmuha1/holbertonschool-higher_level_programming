b50657b13fc6f9e24016b0ea15a07e9ee27bfbd1
Brent Muha <800@holbertonschool.com>
2020-01-13 23:43:18 +0000

    Add github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Use the Github API to list the 10 most recent commits
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[1], argv[2]))
+    for commit in r.json()[0:10]:
+        print('{}: {}'.format(commit.get('sha'), commit.get('commit')
+                              .get('author').get('name')))
