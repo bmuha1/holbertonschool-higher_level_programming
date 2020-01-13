3a68e68f0645e3c93e272f3dd9909a57e5a7b5be
Brent Muha <800@holbertonschool.com>
2020-01-13 23:46:46 +0000

    Switch order of arguments

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(argv[1], argv[2]))
+                     .format(argv[2], argv[1]))
     for commit in r.json()[0:10]:
         print('{}: {}'.format(commit.get('sha'), commit.get('commit')
                               .get('author').get('name')))
