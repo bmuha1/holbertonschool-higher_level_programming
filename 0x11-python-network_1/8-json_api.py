62d497d188d3360b2f65044d6ac977d1930bc8d2
Brent Muha <800@holbertonschool.com>
2020-01-13 21:26:03 +0000

    Add json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Send a POST request to http://0.0.0.0:5000/search_user with letter as a
+parameter
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    if len(argv) == 1:
+        q = ''
+    else:
+        q = argv[1]
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    data = r.json()
+    if not data:
+        print('No result')
+    else:
+        print('[{}] {}'.format(data['id'], data['name']))
