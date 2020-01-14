a7056582875ee718a5ef4e710e094ca832b873f8
Brent Muha <800@holbertonschool.com>
2020-01-14 02:32:24 +0000

    Add Star Wars with films

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Send a search request to the Star Wars API (swapi.co) and manage pagination
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    r = requests.get('https://swapi.co/api/people/?search={}'.format(argv[1]))
+    data = r.json()
+    print('Number of results: {}'.format(data.get('count')))
+    for person in data.get('results'):
+        print(person.get('name'))
+        for film in person.get('films'):
+            f = requests.get(film)
+            print('\t{}'.format(f.json().get('title')))
+    while data.get('next'):
+        r = requests.get(data.get('next'))
+        data = r.json()
+        for person in data.get('results'):
+            print(person.get('name'))
+            for film in person.get('films'):
+                f = requests.get(film)
+                print('\t{}'.format(f.json().get('title')))
