8c4ed02056dcca588677239e0feccdaf8e49be3a
Brent Muha <800@holbertonschool.com>
2020-01-14 04:51:41 +0000

    Refactor

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -7,19 +7,15 @@ if __name__ == '__main__':
     import requests
     from sys import argv
 
-    r = requests.get('https://swapi.co/api/people/?search={}'.format(argv[1]))
-    data = r.json()
-    print('Number of results: {}'.format(data.get('count')))
-    for person in data.get('results'):
+    r = requests.get('https://swapi.co/api/people/?search={}'.format(argv[1])).json()
+    print('Number of results: {}'.format(r.get('count')))
+    for person in r.get('results'):
         print(person.get('name'))
         for film in person.get('films'):
-            f = requests.get(film).json()
-            print('\t{}'.format(f.get('title')))
-    while data.get('next'):
-        r = requests.get(data.get('next'))
-        data = r.json()
-        for person in data.get('results'):
+            print('\t{}'.format(requests.get(film).json().get('title')))
+    while r.get('next'):
+        r = requests.get(r.get('next')).json()
+        for person in r.get('results'):
             print(person.get('name'))
             for film in person.get('films'):
-                f = requests.get(film).json()
-                print('\t{}'.format(f.get('title')))
+                print('\t{}'.format(requests.get(film).json().get('title')))
