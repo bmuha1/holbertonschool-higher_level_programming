bab92431a0556fa55d2dcaa800f80a86acc57be7
Brent Muha <800@holbertonschool.com>
2020-02-04 20:54:35 +0000

    Add parseInt

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,20 +3,20 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   let largest, second;
-  if (process.argv[2] >= process.argv[3]) {
-    largest = process.argv[2];
-    second = process.argv[3];
+  if (parseInt(process.argv[2]) >= parseInt(process.argv[3])) {
+    largest = parseInt(process.argv[2]);
+    second = parseInt(process.argv[3]);
   } else {
-    largest = process.argv[3];
-    second = process.argv[2];
+    largest = parseInt(process.argv[3]);
+    second = parseInt(process.argv[2]);
   }
 
   for (let i = 4; i < process.argv.length; i++) {
-    if (process.argv[i] > largest) {
+    if (parseInt(process.argv[i]) > largest) {
       second = largest;
-      largest = process.argv[i];
-    } else if (process.argv[i] > second) {
-      second = process.argv[i];
+      largest = parseInt(process.argv[i]);
+    } else if (parseInt(process.argv[i]) > second) {
+      second = parseInt(process.argv[i]);
     }
   }
   console.log(second);
