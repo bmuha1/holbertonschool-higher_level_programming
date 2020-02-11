e8f12788d751baf0e850ff65a397f5d5cd26c4fc
Brent Muha <800@holbertonschool.com>
2020-02-11 21:44:45 +0000

    Check each character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,8 +6,10 @@ request(url, function (error, response, body) {
   if (error) throw error;
   let count = 0;
   for (const film of JSON.parse(body).results) {
-    if (film.characters.includes('https://swapi.co/api/people/18/')) {
-      count++;
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        count++;
+      }
     }
   }
   console.log(count);
