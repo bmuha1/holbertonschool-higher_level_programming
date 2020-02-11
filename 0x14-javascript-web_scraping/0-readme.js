01b0069365f6e48ea3dbb3a230edefc6ee6c5d5a
Brent Muha <800@holbertonschool.com>
2020-02-11 17:13:09 +0000

    Add read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
+    return console.log(err);
   }
+  process.stdout.write(data);
 });
